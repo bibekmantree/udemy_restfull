e595847ed65b6147fde719b09f092e1f36c30f9a
bibekmantree <badbond420@gmail.com>
2018-12-03 13:25:46 +0530

    sql tupe crud completion , section 5

--- code/item.py
+++ code/item.py
@@ -22,6 +22,17 @@ class Item(Resource):
         connection.close()
         return row if row else None
 
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES(?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
     @jwt_required()
     def get(self, name):
         row = self.get_item_by_name(name)
@@ -36,25 +47,40 @@ class Item(Resource):
             return {'message': "An item with name {} exists".format(name)}, 400
 
         data = type(self).parser.parse_args()
+        item = {"name": name, "price": data["price"]}
+        try:
+            type(self).insert(item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
+        return item, 201
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = self.get_item_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+        if item is None:
+            try:
+                Item.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item."}
+        else:
+            try:
+                Item.update(updated_item)
+            except:
+                return {"message": "An error occurred updating the item."}
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (name, data['price']))
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
         connection.commit()
         connection.close()
-        return {"item": {"name": name, "price": data['price']}}, 201
-
-
-    def put(self, name):
-        data = type(self).parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -67,5 +93,17 @@ class Item(Resource):
 
 
 class Items(Resource):
+    TABLE_NAME = 'items'
+
     def get(self):
-        return {'items': items}, 200
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+        connection.close()
+
+        return {'items': items}
